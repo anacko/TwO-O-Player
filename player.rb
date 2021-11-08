8ab56e2b27491e8416ef59bda2fd52dceb7d030d
Ana Carolina K <anacko@gmail.com>
2021-11-08 13:03:45 -0500

    create player

---
+++ player.rb
@@ -0,0 +1,13 @@
+class Player
+
+  def initialize(player_number, max_lives = 3)
+    @player = player_number
+    @max_lives = max_lives
+    @current_lives = @max_lives
+  end
+
+  def lose_life
+    @current_lives - 1
+  end
+
+end
